35a398e2e5baaed5e061a73906cf0ad6700f32e5
Ilya Yalymov <chezeee@gmail.com>
2023-01-25 20:12:42 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} years
+ * @param {number} searchYear
+ */
+const getYear = (years, searchYear) => {
+  // your code
+};
+
+// Sample usage - do not modify
+getYear([2019, 2020, 2021], 2020); // 2020
+getYear([2019, 2020, 2021], 1990); // undefined
